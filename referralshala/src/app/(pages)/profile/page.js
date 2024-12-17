9fa74a4ea291e5f6c264bf96424fab5206ef5ecc
Ayush Katiyar <148551722+aerick8602@users.noreply.github.com>
2024-12-17 18:48:19 +0530

    fix path

--- referralshala/src/app/(pages)/profile/page.js
+++ referralshala/src/app/(pages)/profile/page.js
@@ -5,6 +5,46 @@ import Navbar from "../../components/Navbar";
 
 export default function ProfilePage(){
   const { isSignedIn, user, isLoaded } = useUser();
+    const [userType, setUserType] = useState(null);
+    const [userData, setUserData] = useState({});
+
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get(`/api/user/${user.id}`);
+        setUserData(response.data.data);
+      } catch (err) {
+        console.error("Error fetching user data:", err.message);
+      }
+    };
+  
+    const createUserInTable = async () => {
+      if (userType && user.id) {
+        try {
+          const table = userType === 'CANDIDATE' ? 'candidate' : 'employer';
+          const response = await axios.post(`/api/${table}`, {
+            userId: userData.id,
+          });
+          console.log(`${table} created for user ${user.id}`);
+        } catch (err) {
+          console.log("Error creating user in table:", err.message);
+        }
+      }
+    };
+  
+    useEffect(() => {
+      const storedUserType = localStorage.getItem('userType');
+      if (storedUserType) {
+        setUserType(storedUserType);
+      }
+      if (isSignedIn && user?.id) {
+        fetchUserData();
+      }
+    }, [isSignedIn, user]);
+  
+    useEffect(() => {
+      createUserInTable();
+    }, [userData]);
+  
     return (
       <div>
         <Navbar></Navbar>
