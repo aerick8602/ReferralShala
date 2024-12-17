3de5530f8c68b1a5b690f7bae7dbf0882beec780
Ayush Katiyar <148551722+aerick8602@users.noreply.github.com>
2024-12-17 23:40:58 +0530

    router push signout

--- referralshala/src/app/components/Navbar.jsx
+++ referralshala/src/app/components/Navbar.jsx
@@ -1,34 +1,37 @@
 'use client';
 import Link from 'next/link';
-import { SignedIn, SignedOut, SignOutButton } from '@clerk/nextjs';
-import '../styles/Navbar.css'
+import { SignedIn, SignedOut, SignOutButton, useClerk } from '@clerk/nextjs';
+import { useRouter } from 'next/navigation';
+import '../styles/Navbar.css';
 
 export default function Navbar() {
+  const { signOut } = useClerk();
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    await signOut();
+    router.push('/');  
+  };
+
   return (
     <div className="nav-main">
       <div className="nav-icon">
         ReferralShala
       </div>
       <SignedIn>
-      <SignOutButton redirectUrl='/'>
-            <button className="nav-link">Sign Out</button>
-      </SignOutButton>
+        <button onClick={handleSignOut} className="nav-link">
+          Sign Out
+        </button>
       </SignedIn>
       <SignedOut>
-      <div className="nav-links-container">
+        <div className="nav-links-container">
           <Link href="/auth/sign-in" className="nav-link">
             Login
           </Link>
-          <Link
-            href="/auth/sign-up/candidate"
-            className="nav-link"
-          >
+          <Link href="/auth/sign-up/candidate" className="nav-link">
             Candidate Sign-up
           </Link>
-          <Link
-            href="/auth/sign-up/employer"
-            className="nav-link"
-          >
+          <Link href="/auth/sign-up/employer" className="nav-link">
             Employer Sign-up
           </Link>
         </div>
