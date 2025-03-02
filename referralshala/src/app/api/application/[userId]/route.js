e0e13e0599cd5d5b471031d29378275eb2194384
Aditya Sawner <adityasawner19@gmail.com>
2025-03-02 22:23:33 +0530

    10

--- referralshala/src/app/api/application/[userId]/route.js
+++ referralshala/src/app/api/application/[userId]/route.js
@@ -1,58 +1,6 @@
 import { NextResponse } from "next/server";
 import client from "../../../../connection/prisma";
 
-<<<<<<< HEAD
-
-
-  export async function GET(req, { params }) {
-    let { userId } = await params;
-    userId = Number(userId);
-    console.log(userId);
-  
-    try {
-      const applications = await client.application.findMany({
-        where: { userId },
-        select: {
-          referral: {
-            select: {
-              referralId: true,
-              companyName: true,
-              jobTitle: true,
-              jobCategory: true,
-              jobDescription: true,
-              jobLink: true,
-              location: true,
-              experienceRequired: true,
-              postedAt: true,
-              applicationCount:true,
-            },
-          },
-
-        },
-      });
-  
-      if (!applications || applications.length === 0) {
-        return NextResponse.json(
-          { success: false, message: `No referrals found` },
-          { status: 404 }
-        );
-      }
-  
-      console.log(applications);
-  
-      return NextResponse.json(
-        { success: true, data: applications },
-        { status: 200 }
-      );
-    } catch (error) {
-      return NextResponse.json(
-        { success: false, message: error.message },
-        { status: 500 }
-      );
-    }
-  }
-  
-=======
 export async function GET(req, { params }) {
   let { userId } = await params;
   userId = Number(userId);
@@ -99,4 +47,3 @@ export async function GET(req, { params }) {
     );
   }
 }
->>>>>>> e284ee555c29d1d30683fb56e8cf4b8fc9925612
